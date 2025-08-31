ca7985bfaef293389af36cf630431d69dbf9d6be
darahat <darahat42@gmail.com>
2025-08-31 13:37:05 +0600

    Fix: Updated build-and-deploy dev yml file

--- app/Modules/Product/Routes/api.php
+++ app/Modules/Product/Routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Modules\Product\Http\Controllers\ProductController;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('products', ProductController::class);
-});
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::apiResource('products', ProductController::class);
+// });
