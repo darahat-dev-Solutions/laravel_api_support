e290eb884fc49111110124def2719e2ee13a4b75
darahat <darahat42@gmail.com>
2025-09-17 11:12:11 +0600

    Fix: modified aiModuleController index function

--- app/Modules/AiModule/Http/Controllers/AiModuleController.php
+++ app/Modules/AiModule/Http/Controllers/AiModuleController.php
@@ -13,7 +13,7 @@ class AiModuleController extends Controller
      */
     public function index()
     {
-        return AiModule::all();
+        return AiModule::select('id','name')->get();
     }
 
     /**
